4cb938ed1b85177dbd14772495a0fc6d937dba9e
Ben Lackey <ben.lackey@outlook.com>
2018-08-29 17:03:17 -0700

    update to source details

--- simple/compute.tf
+++ simple/compute.tf
@@ -2,10 +2,12 @@ resource "oci_core_instance" "couchbase_server" {
   availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0],"name")}"
   compartment_id      = "${var.tenancy_ocid}"
   display_name        = "couchbase_server"
-  image               = "${var.InstanceImageOCID[var.region]}"
   shape               = "${var.shape}"
   subnet_id           = "${oci_core_subnet.subnet.id}"
-
+  source_details {
+    source_id = "${var.InstanceImageOCID[var.region]}"
+  	source_type = "image"
+  }
   metadata {
     ssh_authorized_keys = "${var.ssh_public_key}"
     user_data           = "${base64encode(file("./scripts/server.sh"))}"
