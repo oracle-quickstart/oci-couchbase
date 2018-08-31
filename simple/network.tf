c437201395b91ea18044835bbd847f5467b3ebe4
Ben Lackey <ben.lackey@outlook.com>
2018-08-30 17:20:34 -0700

    formatting

--- simple/network.tf
+++ simple/network.tf
@@ -14,7 +14,7 @@ resource "oci_core_subnet" "subnet" {
   availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0], "name")}"
   cidr_block          = "10.0.0.0/16"
   display_name        = "subnet"
-  dns_label      = "couchbase"
+  dns_label           = "couchbase"
   vcn_id              = "${oci_core_virtual_network.virtual_network.id}"
   dhcp_options_id     = "${oci_core_virtual_network.virtual_network.default_dhcp_options_id}"
 }
