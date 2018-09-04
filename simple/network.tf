0439e2bc49c9cd2cfee3fdc36087fe16aa3bccc8
Ben Lackey <ben.lackey@outlook.com>
2018-09-04 12:14:16 -0700

    rework subnets...

--- simple/network.tf
+++ simple/network.tf
@@ -5,31 +5,17 @@ data "oci_identity_availability_domains" "availability_domains" {
 resource "oci_core_virtual_network" "virtual_network" {
   cidr_block     = "10.0.0.0/16"
   compartment_id = "${var.tenancy_ocid}"
-  display_name   = "virtual_network"
   dns_label      = "couchbase"
-}
-
-resource "oci_core_subnet" "subnet" {
-  compartment_id      = "${var.tenancy_ocid}"
-  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0], "name")}"
-  cidr_block          = "10.0.0.0/16"
-  display_name        = "subnet"
-  vcn_id              = "${oci_core_virtual_network.virtual_network.id}"
-  dhcp_options_id     = "${oci_core_virtual_network.virtual_network.default_dhcp_options_id}"
-  dns_label           = "couchbase"
-  security_list_ids   = ["${oci_core_security_list.security_list.id}"]
 }
 
 resource "oci_core_internet_gateway" "internet_gateway" {
   compartment_id = "${var.tenancy_ocid}"
-  display_name   = "internet_gateway"
   vcn_id         = "${oci_core_virtual_network.virtual_network.id}"
 }
 
 resource "oci_core_route_table" "route_table" {
   compartment_id = "${var.tenancy_ocid}"
   vcn_id         = "${oci_core_virtual_network.virtual_network.id}"
-  display_name   = "route_table"
 
   route_rules {
     destination       = "0.0.0.0/0"
@@ -39,7 +25,6 @@ resource "oci_core_route_table" "route_table" {
 
 resource "oci_core_security_list" "security_list" {
   compartment_id = "${var.tenancy_ocid}"
-  display_name   = "security_list"
   vcn_id         = "${oci_core_virtual_network.virtual_network.id}"
 
   egress_security_rules = [{
@@ -51,4 +36,15 @@ resource "oci_core_security_list" "security_list" {
     protocol = "All"
     source   = "0.0.0.0/0"
   }]
+}
+
+resource "oci_core_subnet" "subnet" {
+  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0], "name")}"
+  cidr_block          = "10.0.0.0/16"
+  compartment_id      = "${var.tenancy_ocid}"
+  vcn_id              = "${oci_core_virtual_network.virtual_network.id}"
+  route_table_id      = "${oci_core_route_table.route_table.id}"
+  security_list_ids   = ["${oci_core_security_list.security_list.id}"]
+  dhcp_options_id     = "${oci_core_virtual_network.virtual_network.default_dhcp_options_id}"
+  dns_label           = "couchbase"
 }
