5335a40e7c57b3586eb7d9dd8bf74d59a3f16da5
Ben Lackey <ben.lackey@outlook.com>
2018-09-04 12:45:28 -0700

    turns out the default name is terrible

--- simple/network.tf
+++ simple/network.tf
@@ -3,17 +3,20 @@ data "oci_identity_availability_domains" "availability_domains" {
 }
 
 resource "oci_core_virtual_network" "virtual_network" {
-  cidr_block     = "10.0.0.0/16"
+  display_name   = "virtual_network"
   compartment_id = "${var.tenancy_ocid}"
+  cidr_block     = "10.0.0.0/16"
   dns_label      = "couchbase"
 }
 
 resource "oci_core_internet_gateway" "internet_gateway" {
+  display_name   = "internet_gateway"
   compartment_id = "${var.tenancy_ocid}"
   vcn_id         = "${oci_core_virtual_network.virtual_network.id}"
 }
 
 resource "oci_core_route_table" "route_table" {
+  display_name   = "route_table"
   compartment_id = "${var.tenancy_ocid}"
   vcn_id         = "${oci_core_virtual_network.virtual_network.id}"
 
@@ -24,6 +27,7 @@ resource "oci_core_route_table" "route_table" {
 }
 
 resource "oci_core_security_list" "security_list" {
+  display_name   = "security_list"
   compartment_id = "${var.tenancy_ocid}"
   vcn_id         = "${oci_core_virtual_network.virtual_network.id}"
 
@@ -39,9 +43,10 @@ resource "oci_core_security_list" "security_list" {
 }
 
 resource "oci_core_subnet" "subnet" {
+  display_name        = "subnet"
+  compartment_id      = "${var.tenancy_ocid}"
   availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0], "name")}"
   cidr_block          = "10.0.0.0/16"
-  compartment_id      = "${var.tenancy_ocid}"
   vcn_id              = "${oci_core_virtual_network.virtual_network.id}"
   route_table_id      = "${oci_core_route_table.route_table.id}"
   security_list_ids   = ["${oci_core_security_list.security_list.id}"]
