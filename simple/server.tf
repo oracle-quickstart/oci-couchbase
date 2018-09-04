e4259b2c84d5658a10e7ae2abaedf81aa923e4f3
Ben Lackey <ben.lackey@outlook.com>
2018-09-04 15:31:49 -0700

    reorg terraform

---
+++ simple/server.tf
@@ -0,0 +1,28 @@
+resource "oci_core_instance" "couchbase_server" {
+  display_name        = "couchbase_server"
+  compartment_id      = "${var.tenancy_ocid}"
+  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0],"name")}"
+  shape               = "${var.couchbase_server["shape"]}"
+  subnet_id           = "${oci_core_subnet.subnet.id}"
+  source_details {
+    source_id = "${var.images[var.region]}"
+  	source_type = "image"
+  }
+  metadata {
+    ssh_authorized_keys = "${var.ssh_public_key}"
+    user_data           = "${base64encode(file("./scripts/server.sh"))}"
+  }
+  count = "${var.couchbase_server["node_count"]}"
+}
+
+data "oci_core_vnic_attachments" "couchbase_server_vnic_attachments" {
+  compartment_id      = "${var.tenancy_ocid}"
+  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0],"name")}"
+  instance_id         = "${oci_core_instance.couchbase_server.*.id[0]}"
+}
+
+data "oci_core_vnic" "couchbase_server_vnic" {
+  vnic_id = "${lookup(data.oci_core_vnic_attachments.couchbase_server_vnic_attachments.vnic_attachments[0],"vnic_id")}"
+}
+
+output "CouchbaseServerURL" { value = "${data.oci_core_vnic.couchbase_server_vnic.public_ip_address}:8091" }
--- simple/syncgateway.tf
+++ simple/syncgateway.tf
@@ -1,20 +1,3 @@
-resource "oci_core_instance" "couchbase_server" {
-  display_name        = "couchbase_server"
-  compartment_id      = "${var.tenancy_ocid}"
-  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0],"name")}"
-  shape               = "${var.couchbase_server["shape"]}"
-  subnet_id           = "${oci_core_subnet.subnet.id}"
-  source_details {
-    source_id = "${var.images[var.region]}"
-  	source_type = "image"
-  }
-  metadata {
-    ssh_authorized_keys = "${var.ssh_public_key}"
-    user_data           = "${base64encode(file("./scripts/server.sh"))}"
-  }
-  count = "${var.couchbase_server["node_count"]}"
-}
-
 resource "oci_core_instance" "couchbase_syncgateway" {
   display_name        = "couchbase_syncgateway"
   compartment_id      = "${var.tenancy_ocid}"
@@ -31,3 +14,15 @@ resource "oci_core_instance" "couchbase_syncgateway" {
   }
   count = "${var.couchbase_server["node_count"]}"
 }
+
+data "oci_core_vnic_attachments" "couchbase_syncgateway_vnic_attachments" {
+  compartment_id      = "${var.tenancy_ocid}"
+  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0],"name")}"
+  instance_id         = "${oci_core_instance.couchbase_syncgateway.*.id[0]}"
+}
+
+data "oci_core_vnic" "couchbase_syncgateway_vnic" {
+  vnic_id = "${lookup(data.oci_core_vnic_attachments.couchbase_syncgateway_vnic_attachments.vnic_attachments[0],"vnic_id")}"
+}
+
+output "CouchbaseSyncGatewayURL" { value = "${data.oci_core_vnic.couchbase_syncgateway_vnic.public_ip_address}:4985/_admin/" }
