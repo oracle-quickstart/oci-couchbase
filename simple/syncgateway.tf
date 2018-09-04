b928aaf8c0af0e3757ed124c14f004187b0bd9bf
Ben Lackey <ben.lackey@outlook.com>
2018-09-04 15:39:04 -0700

    whoops, wrong script

--- simple/syncgateway.tf
+++ simple/syncgateway.tf
@@ -2,7 +2,7 @@ resource "oci_core_instance" "couchbase_syncgateway" {
   display_name        = "couchbase_syncgateway"
   compartment_id      = "${var.tenancy_ocid}"
   availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0],"name")}"
-  shape               = "${var.couchbase_server["shape"]}"
+  shape               = "${var.couchbase_syncgateway["shape"]}"
   subnet_id           = "${oci_core_subnet.subnet.id}"
   source_details {
     source_id = "${var.images[var.region]}"
@@ -10,9 +10,9 @@ resource "oci_core_instance" "couchbase_syncgateway" {
   }
   metadata {
     ssh_authorized_keys = "${var.ssh_public_key}"
-    user_data           = "${base64encode(file("./scripts/server.sh"))}"
+    user_data           = "${base64encode(file("./scripts/syncgateway.sh"))}"
   }
-  count = "${var.couchbase_server["node_count"]}"
+  count = "${var.couchbase_syncgateway["node_count"]}"
 }
 
 data "oci_core_vnic_attachments" "couchbase_syncgateway_vnic_attachments" {
