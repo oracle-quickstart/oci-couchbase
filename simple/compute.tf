bf19e12904b77b8bfd5f08dced54d4b936ea2b4e
Ben Lackey <ben.lackey@outlook.com>
2018-09-04 15:26:46 -0700

    bad array ref

--- simple/compute.tf
+++ simple/compute.tf
@@ -2,7 +2,7 @@ resource "oci_core_instance" "couchbase_server" {
   display_name        = "couchbase_server"
   compartment_id      = "${var.tenancy_ocid}"
   availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0],"name")}"
-  shape               = "${var.couchbase_server[shape]}"
+  shape               = "${var.couchbase_server["shape"]}"
   subnet_id           = "${oci_core_subnet.subnet.id}"
   source_details {
     source_id = "${var.images[var.region]}"
@@ -12,14 +12,14 @@ resource "oci_core_instance" "couchbase_server" {
     ssh_authorized_keys = "${var.ssh_public_key}"
     user_data           = "${base64encode(file("./scripts/server.sh"))}"
   }
-  count = "${var.couchbase_server[node_count]}"
+  count = "${var.couchbase_server["node_count"]}"
 }
 
 resource "oci_core_instance" "couchbase_syncgateway" {
   display_name        = "couchbase_syncgateway"
   compartment_id      = "${var.tenancy_ocid}"
   availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0],"name")}"
-  shape               = "${var.couchbase_server[shape]}"
+  shape               = "${var.couchbase_server["shape"]}"
   subnet_id           = "${oci_core_subnet.subnet.id}"
   source_details {
     source_id = "${var.images[var.region]}"
@@ -29,5 +29,5 @@ resource "oci_core_instance" "couchbase_syncgateway" {
     ssh_authorized_keys = "${var.ssh_public_key}"
     user_data           = "${base64encode(file("./scripts/server.sh"))}"
   }
-  count = "${var.couchbase_server[node_count]}"
+  count = "${var.couchbase_server["node_count"]}"
 }
