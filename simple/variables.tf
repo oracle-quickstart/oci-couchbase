44d7aa438901e94e0fb28dd26ea4e531f01b4a1a
Ben Lackey <ben.lackey@outlook.com>
2018-09-04 16:15:05 -0700

    region in env vars

--- simple/variables.tf
+++ simple/variables.tf
@@ -9,7 +9,7 @@ variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
-variable "region" { default = "eu-frankfurt-1" }
+variable "region" {}
 
 # Key used to SSH to OCI VMs
 variable "ssh_public_key" {}
