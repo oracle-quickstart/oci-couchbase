fdc2fab5af5af362cb6f9cb515868ab3bb2f7b2c
Ben Lackey <ben.lackey@outlook.com>
2018-09-11 00:54:19 -0400

    missed that one

--- simple/server.tf
+++ simple/server.tf
@@ -11,7 +11,7 @@ resource "oci_core_instance" "couchbase_server" {
   metadata {
     ssh_authorized_keys = "${var.ssh_public_key}"
     user_data           = "${base64encode(format("%s\n%s\n%s\n%s\n",
-      "version=", "${var.couchbase_server["version"]}",
+      "version=${var.couchbase_server["version"]}",
       "adminUsername=${var.couchbase_server["adminUsername"]}",
       "adminPassword=${var.couchbase_server["adminPassword"]}",
       file("../scripts/server.sh")
