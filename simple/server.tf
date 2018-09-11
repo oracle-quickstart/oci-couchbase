332ee631e652846f278bb6ceb36405dc877c30f0
Ben Lackey <ben.lackey@outlook.com>
2018-09-11 00:13:29 -0400

    params

--- simple/server.tf
+++ simple/server.tf
@@ -10,7 +10,12 @@ resource "oci_core_instance" "couchbase_server" {
   }
   metadata {
     ssh_authorized_keys = "${var.ssh_public_key}"
-    user_data           = "${base64encode(file("../scripts/server.sh"))}"
+    user_data           = "${base64encode(format("%s\n%s%s\n%s%s\n%s%s\n",
+      file("../scripts/server.sh"),
+      "version=", ${var.couchbase_server["version"]},
+      "adminUsername=", ${var.couchbase_server["adminUsername"]},
+      "adminPassword=", ${var.couchbase_server["adminPassword"]},
+    ))}"
   }
   count = "${var.couchbase_server["node_count"]}"
 }
