c30832203d100d0b808d02b984d3665150d37701
Ben Lackey <ben.lackey@outlook.com>
2018-09-11 01:49:33 -0400

    wrong variable

--- simple/syncgateway.tf
+++ simple/syncgateway.tf
@@ -12,7 +12,7 @@ resource "oci_core_instance" "couchbase_syncgateway" {
     ssh_authorized_keys = "${var.ssh_public_key}"
     user_data           = "${base64encode(format("%s\n%s\n%s\n",
       "#!/usr/bin/env bash",
-      "version=${var.couchbase_server["version"]}",
+      "version=${var.couchbase_syncgateway["version"]}",
       file("../scripts/syncgateway.sh")
     ))}"
   }
